797fd965c4d262cc34c710839698b03db079de16
Wellsx <stefanveleusic@hotmail.com>
2023-07-18 14:17:52 +0200

    feat(refactor): removed utils package, added rsakey generation to security config class

--- src/main/java/com/stefan/library/app/configuration/SecurityConfiguration.java
+++ src/main/java/com/stefan/library/app/configuration/SecurityConfiguration.java
@@ -1,9 +1,11 @@
 package com.stefan.library.app.configuration;
 
-import java.util.HashMap;
-import java.util.Map;
 
-import com.stefan.library.app.utils.RSAKeyProperties;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -24,33 +26,28 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class SecurityConfiguration {
+    private final KeyPair keyPair;
 
-    private final RSAKeyProperties keys;
-
-    public SecurityConfiguration(RSAKeyProperties keys) {
-        this.keys = keys;
+    public SecurityConfiguration() throws NoSuchAlgorithmException {
+        this.keyPair = generateRsaKeyPair();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-
         String idForEncode = "bcrypt";
-
         Map<String, PasswordEncoder> encoderMap = new HashMap<>();
-
         encoderMap.put(idForEncode, new BCryptPasswordEncoder());
-
         return new DelegatingPasswordEncoder(idForEncode, encoderMap);
-
     }
 
     @Bean
@@ -60,14 +57,11 @@ public class SecurityConfiguration {
         return new ProviderManager(daoProvider);
     }
 
-    // method that returns security chain takes in http security
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
-
                     auth.requestMatchers("/auth/**").permitAll();
                     auth.requestMatchers("/admin/**").hasRole("ADMIN");
                     auth.requestMatchers("/user/**").hasAnyRole("ADMIN", "USER");
@@ -81,31 +75,38 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
-
     }
 
-    // gets JWT and decodes data
     @Bean
     public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withPublicKey(keys.getPublicKey()).build();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
     }
 
     @Bean
     public JwtEncoder jwtEncoder() {
-        JWK jwk = new RSAKey.Builder(keys.getPublicKey()).privateKey(keys.getPrivateKey()).build();
-        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
-        return new NimbusJwtEncoder(jwks);
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        RSAKey rsaKey = new RSAKey.Builder(publicKey).privateKey(privateKey).build();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(jwkSet);
+        return new NimbusJwtEncoder(jwkSource);
     }
+
 
     @Bean
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        // Convert roles into ROLE_ so JWT can handle roles properly
         JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
         jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
         jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
         JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
         jwtConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
         return jwtConverter;
+    }
 
+    private KeyPair generateRsaKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        return keyPairGenerator.generateKeyPair();
     }
 }
--- src/main/java/com/stefan/library/app/services/TokenService.java
+++ src/main/java/com/stefan/library/app/services/TokenService.java
@@ -2,8 +2,6 @@ package com.stefan.library.app.services;
 
 import java.time.Instant;
 import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
